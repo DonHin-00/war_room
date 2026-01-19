f0d59b79cec2e86efe7514f5600242f675292db7
DonHin <hinojosadonovan05@gmail.com>
2026-01-18 19:31:42 -0600

    Create centralized configuration file for Red and Blue teams

---
+++ config.py
@@ -0,0 +1,35 @@
+# Centralized Configuration for Red and Blue Teams
+
+# Hyperparameters
+hyperparameters = {
+    'learning_rate': 0.001,
+    'num_episodes': 1000,
+    'discount_factor': 0.99,
+}
+
+# Rewards
+rewards = {
+    'victory': 10,
+    'defeat': -10,
+    'draw': 5,
+}
+
+# Actions
+actions = [
+    'attack',
+    'defend',
+    'retreat',
+    'gather_information'
+]
+
+# File Paths
+file_paths = {
+    'model_save_path': './models/',
+    'log_file_path': './logs/train.log',
+}
+
+# Logging Settings
+logging_settings = {
+    'log_level': 'INFO',
+    'log_format': '%(asctime)s - %(levelname)s - %(message)s',
+}
